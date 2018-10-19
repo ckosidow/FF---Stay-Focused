f6a40f460d55d44b85768d50177c4c70163f14ed
ckosidow <ckosidow@fastenal.com>
2018-10-19 09:05:25 -0500

    Trying to fix bug where the timer doesn't reset if firefox is closed at midnight

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,25 @@
+let ls = browser.storage.local;
+
+ls.get().then(function(res) {
+    let now = new Date();
+    now.setHours(0, 0, 0, 0);
+
+    if (res['presentBlockingDay'] !== now.valueOf()) {
+        ls.set({presentBlockingDay: now.valueOf()});
+        ls.set({site_timer: 0});
+    }
+});
+
 function setForTomorrow() {
     let now = new Date();
     let millisTill00 = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0, 0) - now; // ms until 00:00 tomorrow
 
     setTimeout(function() {
-        browser.storage.local.set({site_timer: 0});
+        let now = new Date();
+        now.setHours(0, 0, 0, 0);
+
+        ls.set({presentBlockingDay: now.valueOf()});
+        ls.set({site_timer: 0});
 
         setForTomorrow();
     }, millisTill00);
